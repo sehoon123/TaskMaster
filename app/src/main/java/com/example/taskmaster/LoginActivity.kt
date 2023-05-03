86bd931da47fbc9a7a9168819cbdbcad5194bac6
SH Jung <60769447+sehoon123@users.noreply.github.com>
2023-05-04 00:45:09 +0900

    login feature added (#3)
    * init
    
    * protection test
    
    * gradle update
    
    * login feature added

---
+++ app/src/main/java/com/example/taskmaster/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.example.taskmaster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.example.taskmaster.MainActivity
+import com.example.taskmaster.databinding.ActivityLogin2Binding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogin2Binding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        handleSignInResult(task)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize FirebaseAuth instance
+        auth = FirebaseAuth.getInstance()
+
+        // Replace with your server's client ID
+        val serverClientId = "776882834377-uje7bpc9e0dauorrt24qh8nk5r7fcdur.apps.googleusercontent.com"
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(serverClientId)
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.SignInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startForResult.launch(signInIntent)
+    }
+
+    private fun handleSignInResult(completedTask: com.google.android.gms.tasks.Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(this, "Signed in successfully", Toast.LENGTH_SHORT).show()
+
+            // Authenticate with Firebase
+            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Start MainActivity and finish LoginActivity
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        Toast.makeText(this, "Authentication Failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } catch (e: ApiException) {
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            Toast.makeText(this, "Failed to sign in", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
+}
--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.taskmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if the user is signed in
+        val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+        if (account == null) {
+            // If the user is not signed in, start LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
