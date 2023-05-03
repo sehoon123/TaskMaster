ef793d4b03becd5f9557f2c092e3f655332f9683
sehoon <sehoon0519@naver.com>
2023-05-04 02:04:22 +0900

    db connection test

--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -3,9 +3,18 @@ package com.example.taskmaster
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
+    private lateinit var myRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +27,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        database = FirebaseDatabase.getInstance()
+        myRef = database.getReference("message")
+
+        // Write data to the Realtime Database
+        myRef.setValue("Hello, World!")
+
+        // Read data from the Realtime Database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d("MainActivity", "Value is: $value")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("MainActivity", "Failed to read value.", databaseError.toException())
+            }
+        })
     }
 }
