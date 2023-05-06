e89f58fd1a00052efa022e4a3b6195d7d44841c3
sehoon <sehoon0519@naver.com>
2023-05-07 02:14:25 +0900

    taskmaster v0.0.2

--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.example.taskmaster.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: FirebaseDatabase
     private lateinit var myRef: DatabaseReference
     private lateinit var todoAdapter: TodoAdapter
-    private val todoList = mutableListOf<String>()
+    private val todoList = mutableListOf<Todo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,29 +30,37 @@ class MainActivity : AppCompatActivity() {
 
         setupAddButton()
 
-        setupCameraButton()
-
         readTodosFromDatabase()
     }
 
     private fun initializeDatabaseReference() {
-        // Check if the user is signed in
         val user = FirebaseAuth.getInstance().currentUser
         if (user == null) {
-            // If the user is not signed in, start LoginActivity
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         } else {
-            // User is signed in, initialize the database reference for the current user
             val userId = user.uid
+            val currentDate = getCurrentDateString()
             database = FirebaseDatabase.getInstance()
-            myRef = database.getReference("todos").child(userId)
+            myRef = database.getReference("todos").child(userId).child(currentDate)
         }
     }
 
+    private fun getCurrentDateString(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(Date())
+    }
+
+
     private fun setupRecyclerView() {
-        todoAdapter = TodoAdapter(todoList)
+        todoAdapter = TodoAdapter(todoList, { position ->
+            showUpdateDeleteDialog(position)
+        }) { position, isChecked ->
+            val todo = todoList[position]
+            val updatedTodo = todo.copy(isChecked = isChecked as Boolean)
+            updateTodoInDatabase(todo.key, updatedTodo)
+        }
         binding.rvTodos.adapter = todoAdapter
         binding.rvTodos.layoutManager = LinearLayoutManager(this)
     }
@@ -63,37 +74,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setupCameraButton() {
-        binding.btnCamera.setOnClickListener {
-            launchCameraActivity()
-        }
-    }
+    private fun showUpdateDeleteDialog(position: Int) {
+        val options = arrayOf("Update", "Delete")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Choose an action")
+        builder.setItems(options) { _, which ->
+            when (which) {
+                0 -> {
+                    val intent = Intent(this, ViewActivity::class.java)
+                    intent.putExtra("action", "update")
+                    intent.putExtra("todoKey", todoList[position].key)
+                    intent.putExtra("todoTitle", todoList[position].title)
+                    startActivity(intent)
+                }
 
-    private fun launchCameraActivity() {
-        val intent = Intent(this, CameraActivity::class.java)
-        startActivity(intent)
+                1 -> {
+                    val todo = todoList[position]
+                    deleteTodoFromDatabase(todo.key)
+                }
+            }
+        }
+        builder.show()
     }
 
     private fun addNewTodoToDatabase(newTodo: String) {
         val key = myRef.push().key
         key?.let {
-            myRef.child(it).setValue(newTodo)
+            myRef.child(it).setValue(Todo(newTodo))
             binding.etTodo.setText("")
         }
     }
 
-
     private fun readTodosFromDatabase() {
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val newTodoList = mutableListOf<String>()
+                val newTodoList = mutableListOf<Todo>()
 
                 for (childSnapshot in dataSnapshot.children) {
-                    val todoData = childSnapshot.getValue()
-                    if (todoData is String) {
-                        newTodoList.add(todoData)
-                    } else {
-                        Log.w("MainActivity", "Invalid todo data format: $todoData")
+                    val todoData = childSnapshot.getValue(Todo::class.java)
+                    if (todoData != null) {
+                        val key = childSnapshot.key
+                        val todoWithKey = todoData.copy(key = key)
+                        newTodoList.add(todoWithKey)
                     }
                 }
 
@@ -106,6 +128,19 @@ class MainActivity : AppCompatActivity() {
                 Log.w("MainActivity", "Failed to read value.", databaseError.toException())
             }
         })
+    }
+
+    private fun updateTodoInDatabase(key: String?, updatedTodo: Todo) {
+        key?.let {
+            myRef.child(it).setValue(updatedTodo)
+        }
+    }
+
+
+    private fun deleteTodoFromDatabase(key: String?) {
+        key?.let {
+            myRef.child(it).removeValue()
+        }
     }
 
 }
