34035473d337c82a2358fd8bf2ff284855a7eb7e
sehoon <sehoon0519@naver.com>
2023-05-14 03:29:40 +0900

    optimize google map speed

--- app/src/main/java/com/example/taskmaster/ViewFragment.kt
+++ app/src/main/java/com/example/taskmaster/ViewFragment.kt
@@ -1,153 +1,135 @@
 package com.example.taskmaster
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings.Global.getString
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.taskmaster.databinding.FragmentViewBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import java.text.SimpleDateFormat
-import java.util.*
-import android.location.Geocoder
-import android.location.Location
-import androidx.core.location.LocationManagerCompat.getCurrentLocation
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class ViewFragment : Fragment() {
+class ViewFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentViewBinding
-    private lateinit var database: FirebaseDatabase
-    private lateinit var myRef: DatabaseReference
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var mapView: MapView
-    private var googleMap: GoogleMap? = null
+    private lateinit var googleMap: GoogleMap
+
+    private lateinit var database: DatabaseReference
+    private var todoKey: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentViewBinding.inflate(inflater, container, false)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-        initializeMapView(savedInstanceState)
-        initializeDatabaseReference()
-        handleTodoLogic()
         return binding.root
     }
 
-    private fun initializeMapView(savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Retrieve todo key from arguments
+        todoKey = arguments?.getString("todoKey")
+
         mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync { map ->
-            googleMap = map.apply {
-                uiSettings.run {
-                    isZoomControlsEnabled = true
-                    isZoomGesturesEnabled = true
-                    isScrollGesturesEnabled = true
-                    isTiltGesturesEnabled = true
-                }
-            }
-            showTodoLocationOnMap()
-        }
-    }
+        mapView.getMapAsync(this)
 
-    private fun handleTodoLogic() {
-        val action = arguments?.getString("action")
-        val todoKey = arguments?.getString("todoKey")
-
-        if (todoKey != null) {
-            myRef.child(todoKey).addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val todo = snapshot.getValue(Todo::class.java)
-                    Log.d("ViewFragment", "Snapshot: $snapshot")
-                    binding.titleText.setText(todo?.title)
-
-                    todo?.let { showTodoLocation(it) }
-
-                    when (action) {
-                        "update" -> setupUpdateLogic(todo)
-                        "delete" -> setupDeleteLogic(todo)
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Error reading todo data: ${error.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    requireActivity().onBackPressed()
-                }
-            })
-        } else {
-            Toast.makeText(requireContext(), R.string.todo_key_not_found, Toast.LENGTH_SHORT).show()
-            requireActivity().onBackPressed()
-        }
-    }
-
-    private fun showTodoLocationOnMap() {
-        val latitude = arguments?.getDouble("latitude")
-        val longitude = arguments?.getDouble("longitude")
-
-        if (latitude != null && longitude != null && googleMap != null) {
-            val location = LatLng(latitude, longitude)
-            googleMap?.addMarker(MarkerOptions().position(location))
-            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
-        }
-    }
-
-    private fun initializeDatabaseReference() {
-        val user = FirebaseAuth.getInstance().currentUser
-        if (user == null) {
-            Toast.makeText(requireContext(), "User is not signed in", Toast.LENGTH_SHORT).show()
-            requireActivity().finish()
-            return
-        }
-
-        val userId = user.uid
-        val currentDate = getCurrentDateString()
-        database = FirebaseDatabase.getInstance()
-        myRef = database.getReference("todos").child(userId).child(currentDate)
+        // Initialize Firebase Realtime Database reference
+        database = FirebaseDatabase.getInstance().reference.child("todos").child(FirebaseAuth.getInstance().currentUser!!.uid).child(getCurrentDateString())
+        Log.d("ViewFragment", "Database reference: $database")
     }
 
     private fun getCurrentDateString(): String {
-        val sdf: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val sdf : SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         return sdf.format(Date())
     }
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        // Retrieve location data from Firebase Realtime Database
+        if (todoKey != null) {
+            val todoRef = database.child(todoKey!!)
+            todoRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val todo = dataSnapshot.getValue(Todo::class.java)
+                    Log.d("ViewFragment", "Snapshot: $dataSnapshot")
+                    binding.titleText.setText(todo?.title)
+
+                    if (todo != null && todo.latitude != null && todo.longitude != null) {
+                        // Customize the map and add markers, polylines, etc.
+                        // Here's an example of adding a marker at a specific location
+                        val markerOptions = MarkerOptions()
+                            .position(LatLng(todo.latitude, todo.longitude))
+                            .title("Todo Location")
+                        googleMap.addMarker(markerOptions)
+
+                        // Move the camera to the marker's position
+                        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                            LatLng(todo.latitude, todo.longitude),
+                            12f
+                        )
+                        googleMap.animateCamera(cameraUpdate)
+                    }
+
+                    setupUpdateLogic(todo)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Handle database error
+                    Toast.makeText(requireContext(), "Failed to retrieve todo data", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
 
     private fun setupUpdateLogic(todo: Todo?) {
         binding.btnSave.setOnClickListener { updateTodoInDatabase(todo) }
         binding.btnCancel.setOnClickListener { requireActivity().onBackPressed() }
     }
-
     private fun updateTodoInDatabase(todo: Todo?) {
         val title = binding.titleText.text.toString()
         if (todo != null) {
             val updatedTodo = todo.copy(title = title)
             todo.key?.let { key ->
-                myRef.child(key).setValue(updatedTodo)
+                database.child(key).setValue(updatedTodo)
                     .addOnSuccessListener {
                         Toast.makeText(
                             requireContext(),
@@ -168,134 +150,4 @@ class ViewFragment : Fragment() {
             Toast.makeText(requireContext(), R.string.todo_key_not_found, Toast.LENGTH_SHORT).show()
         }
     }
-
-    private fun setupDeleteLogic(todo: Todo?) {
-        binding.btnSave.text = getString(R.string.delete)
-        binding.btnSave.setOnClickListener { deleteTodoFromDatabase(todo) }
-        binding.btnCancel.setOnClickListener { requireActivity().onBackPressed() }
-    }
-
-    private fun deleteTodoFromDatabase(todo: Todo?) {
-        if (todo != null) {
-            todo.key?.let { key ->
-                myRef.child(key).removeValue()
-                    .addOnSuccessListener {
-                        Toast.makeText(
-                            requireContext(),
-                            R.string.todo_deleted_successfully,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        requireActivity().onBackPressed()
-                    }
-                    .addOnFailureListener { e ->
-                        Toast.makeText(
-                            requireContext(),
-                            "Error deleting todo: ${e.localizedMessage}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-            }
-        } else {
-            Toast.makeText(requireContext(), R.string.todo_key_not_found, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun showTodoLocation(todo: Todo) {
-        val latitude = todo.latitude
-        val longitude = todo.longitude
-        Log.d("ViewFragment", "showTodoLocation: $latitude, $longitude")
-
-        if (latitude != null && longitude != null && googleMap != null) {
-            val location = LatLng(latitude, longitude)
-            googleMap?.addMarker(MarkerOptions().position(location))
-            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
-        } else {
-            Toast.makeText(requireContext(), "Location is not available", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocation()
-            } else {
-                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }
-
-    private fun requestCurrentLocation() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
-        } else {
-            getCurrentLocation()
-        }
-    }
-
-    private fun getCurrentLocation() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
-        } else {
-            try {
-                fusedLocationClient.lastLocation
-                    .addOnSuccessListener { location ->
-                        if (location != null) {
-                            saveLocationToTodo(location)
-                        } else {
-                            Toast.makeText(
-                                requireContext(),
-                                "Failed to get current location",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                    .addOnFailureListener { e ->
-                        Toast.makeText(
-                            requireContext(),
-                            "Error getting current location: ${e.localizedMessage}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-            } catch (e: SecurityException) {
-                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }
-
-    private fun saveLocationToTodo(location: Location) {
-        val latitude = location.latitude
-        val longitude = location.longitude
-        val todoKey = arguments?.getString("todoKey")
-        if (todoKey != null) {
-            myRef.child(todoKey).child("latitude").setValue(latitude)
-            myRef.child(todoKey).child("longitude").setValue(longitude)
-            Toast.makeText(requireContext(), "Location saved to todo", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    companion object {
-        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
-    }
-
 }
