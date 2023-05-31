ad4910f98b155288d2a5c233e43f1e11b977c563
sehoon <sehoon0519@naver.com>
2023-05-31 23:50:50 +0900

    fix location gps

--- app/src/main/java/com/example/taskmaster/Weather2Fragment.kt
+++ app/src/main/java/com/example/taskmaster/Weather2Fragment.kt
@@ -1,11 +1,18 @@
 package com.example.taskmaster
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -17,6 +24,10 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class Weather2Fragment : Fragment() {
+
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSION = 1
+    }
 
     private lateinit var progressBar: ProgressBar
     private lateinit var weatherTextView: TextView
@@ -39,17 +50,74 @@ class Weather2Fragment : Fragment() {
 
     private fun fetchWeatherData() {
         val apiKey = "4964a25342d6c470365520fa7b81a53c"
-        val city = "Seoul" // Replace with the desired city name
 
-        val apiUrl =
-            "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric"
+        val locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        GlobalScope.launch(Dispatchers.Main) {
-            progressBar.visibility = View.VISIBLE
-            val result = withContext(Dispatchers.IO) { URL(apiUrl).readText() }
-            progressBar.visibility = View.GONE
+        // Check for location permission
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request location permission if not granted
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+            return
+        }
 
-            parseWeatherData(result)
+        // Get the last known location
+        val lastKnownLocation =
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (lastKnownLocation != null) {
+            // Fetch weather data using GPS coordinates
+            val latitude = lastKnownLocation.latitude
+            val longitude = lastKnownLocation.longitude
+
+            val apiUrl =
+                "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric"
+
+            GlobalScope.launch(Dispatchers.Main) {
+                progressBar.visibility = View.VISIBLE
+                val result = withContext(Dispatchers.IO) { URL(apiUrl).readText() }
+                progressBar.visibility = View.GONE
+
+                parseWeatherData(result)
+            }
+        } else {
+            // If last known location is not available, request location updates
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                object : LocationListener {
+                    override fun onLocationChanged(location: Location) {
+                        // Fetch weather data using GPS coordinates
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+
+                        val apiUrl =
+                            "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric"
+
+                        GlobalScope.launch(Dispatchers.Main) {
+                            progressBar.visibility = View.VISIBLE
+                            val result = withContext(Dispatchers.IO) { URL(apiUrl).readText() }
+                            progressBar.visibility = View.GONE
+
+                            parseWeatherData(result)
+                        }
+
+                        // Stop further location updates
+                        locationManager.removeUpdates(this)
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+                }
+            )
         }
     }
 
@@ -64,5 +132,18 @@ class Weather2Fragment : Fragment() {
 
         val weatherInfo = "Temperature: $temperature°C\nHumidity: $humidity%\nUpdated at: $dateTime"
         weatherTextView.text = weatherInfo
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, fetch weather data
+                fetchWeatherData()
+            }
+        }
     }
 }
